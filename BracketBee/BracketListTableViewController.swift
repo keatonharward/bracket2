41083fa7dff4de45a9937c8c08087599fa192042
Keaton Harward <keatonharward@gmail.com>
2017-03-06 11:38:32 -0700

    Bracket list TableView

---
+++ BracketBee/BracketListTableViewController.swift
@@ -0,0 +1,92 @@
+//
+//  BracketListTableViewController.swift
+//  BracketBee
+//
+//  Created by Keaton Harward on 3/6/17.
+//  Copyright © 2017 Keaton Harward. All rights reserved.
+//
+
+import UIKit
+
+class BracketListTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.tableView.separatorColor = Keys.shared.accent
+        self.tableView.backgroundColor = Keys.shared.background
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return BracketController.shared.brackets.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "bracketCell", for: indexPath)
+
+        let bracket = BracketController.shared.brackets[indexPath.row]
+        cell.textLabel?.text = bracket.name
+        cell.textLabel?.textColor = Keys.shared.fontColor
+        cell.backgroundColor = Keys.shared.alternateBackground
+        
+        // TODO: - Set detail text label
+
+        return cell
+    }
+ 
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ BracketBee/Keys.swift
@@ -0,0 +1,20 @@
+//
+//  Keys.swift
+//  BracketBee
+//
+//  Created by Keaton Harward on 3/6/17.
+//  Copyright © 2017 Keaton Harward. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Keys {
+    static let shared = Keys()
+    
+    let background = UIColor(colorLiteralRed: 37 / 255, green: 37 / 255, blue: 37 / 255, alpha: 1)
+    let alternateBackground = UIColor(colorLiteralRed: 71 / 255, green: 71 / 255, blue: 71 / 255, alpha: 1)
+    let accent = UIColor(colorLiteralRed: 89 / 255, green: 214 / 255, blue: 255 / 255, alpha: 1)
+    let fontColor = UIColor.white
+    let greyAccent = UIColor(colorLiteralRed: 151 / 255, green: 151 / 255, blue: 151 / 255, alpha: 1)
+}
